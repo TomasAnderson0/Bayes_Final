56869548437f749e3e8804dd1b2f79066ea78c01
Tomas <tomas.anderson508375@gmail.com>
2024-06-17 18:49:52 -0300

    version over 9000

--- Modelos.R
+++ Modelos.R
@@ -18,10 +18,10 @@ lista <- list(N = nrow(cerebros),
               diag = (as.numeric(as.factor(cerebros$diag)) - 1),
               age = cerebros$edad,
               sex = as.numeric(as.factor(cerebros$sexo)),
-              vhi = cerebros$lh_subcx_hippocampus_volume/1000,
-              vi = cerebros$xh_general_etiv_volume/100000,
+              vhi = cerebros$lh_subcx_hippocampus_volume,
+              vi = cerebros$xh_general_etiv_volume,
               ecsf = cerebros$lh_cortex_superiorfrontal_thickness,
-              vcf = cerebros$lh_cortex_fusiform_volume/1000,
+              vcf = cerebros$lh_cortex_fusiform_volume,
               inte = as.numeric(as.factor(cerebros$intensidad_campo)),
               res = as.numeric(as.factor(cerebros$resonador_fab))
 )

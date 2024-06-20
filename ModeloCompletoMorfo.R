73b94089960682975b9087d4a3b90c92f7362040
AlejoVaschetti <alejovaschetti@hotmail.com>
2024-06-20 16:08:17 -0300

    Gráfico betas modelo regresión lineal completo

--- ModeloCompletoMorfo.R
+++ ModeloCompletoMorfo.R
@@ -29,8 +29,15 @@ modeloregcomp <- stan(
 )
 
 posteriorcomp <- data.frame(extract(modeloregcomp, c("beta1", "beta2", "beta3", "beta4", "beta5")))
+posteriorcomp <- data.frame(b1 = c(posteriorcomp$beta1, posteriorcomp$beta2, posteriorcomp$beta3,
+                                   posteriorcomp$beta4, posteriorcomp$beta5, posteriorcomp$beta6,
+                                   posteriorcomp$beta7))
+posteriorcomp <- posteriorcomp %>% 
+  mutate(variable = c(rep("Edad", 5400), rep("Sexo", 5400), rep("Resonador GE", 5400), rep("Resonador Philips", 5400), rep("Intensidad", 5400)))
 
-ggplot(posteriorcomp) + geom_density(aes(x = beta5))
+ggplot(posteriorcomp) + stat_summary(aes(y = variable, x = b1), fun.data = mean_sdl, color = "#057057") + 
+  geom_vline(xintercept = 0, linetype = 2) + labs(y = "Variable", x = "Coeficiente") + theme_minimal()
+
 
 # ---------------------------------------------------------------------
 
@@ -53,9 +60,15 @@ modeloregcomp2 <- stan(
   seed = 1997
 )
 
-posteriorcomp2 <- data.frame(extract(modeloregcomp2, c("beta1", "beta3", "beta4", "beta5")))
+posteriorcomp2 <- data.frame(extract(modeloregcomp2, c("beta1", "beta2", "beta3", "beta4", "beta5")))
+posteriorcomp2 <- data.frame(b1 = c(posteriorcomp2$beta1, posteriorcom2p$beta2, posteriorcomp2$beta3,
+                                   posteriorcomp2$beta4, posteriorcomp2$beta5, posteriorcomp2$beta6,
+                                   posteriorcomp2$beta7))
+posteriorcomp2 <- posteriorcomp2 %>% 
+  mutate(variable = c(rep("Edad", 5400), rep("Sexo", 5400), rep("Resonador GE", 5400), rep("Resonador Philips", 5400), rep("Intensidad", 5400)))
 
-ggplot(posteriorcomp2) + geom_density(aes(x = beta1))
+ggplot(posteriorcomp2) + stat_summary(aes(y = variable, x = b1), fun.data = mean_sdl, color = "#057057") + 
+  geom_vline(xintercept = 0, linetype = 2) + labs(y = "Variable", x = "Coeficiente") + theme_minimal()
 
 # --------------------------------------------------------------
 
@@ -78,9 +91,17 @@ modeloregcomp3 <- stan(
   seed = 1997
 )
 
-posteriorcomp3 <- data.frame(extract(modeloregcomp3, c("beta3", "beta4", "beta5")))
+traceplot(modeloregcomp3)
 
-ggplot(posteriorcomp3) + geom_density(aes(x = beta5))
+posteriorcomp3 <- data.frame(extract(modeloregcomp3, c("beta1", "beta2", "beta3", "beta4", "beta5")))
+posteriorcomp3 <- data.frame(b1 = c(posteriorcomp3$beta1, posteriorcomp3$beta2, posteriorcomp3$beta3,
+                                    posteriorcomp3$beta4, posteriorcomp3$beta5, posteriorcomp3$beta6,
+                                    posteriorcomp3$beta7))
+posteriorcomp3 <- posteriorcomp3 %>% 
+  mutate(variable = c(rep("Edad", 5400), rep("Sexo", 5400), rep("Resonador GE", 5400), rep("Resonador Philips", 5400), rep("Intensidad", 5400)))
+
+ggplot(posteriorcomp3) + stat_summary(aes(y = variable, x = b1), fun.data = mean_sdl, color = "#057057") + 
+  geom_vline(xintercept = 0, linetype = 2) + labs(y = "Variable", x = "Coeficiente") + theme_minimal()
 
 # ---------------------------------------------------------------------------------
 
@@ -103,7 +124,14 @@ modeloregcomp4 <- stan(
   seed = 1997
 )
 
-posteriorcomp4 <- data.frame(extract(modeloregcomp4, c("beta3", "beta4", "beta5")))
+posteriorcomp4 <- data.frame(extract(modeloregcomp4, c("beta1", "beta2", "beta3", "beta4", "beta5")))
+posteriorcomp4 <- data.frame(b1 = c(posteriorcomp4$beta1, posteriorcomp4$beta2, posteriorcomp4$beta3,
+                                    posteriorcomp4$beta4, posteriorcomp4$beta5, posteriorcomp4$beta6,
+                                    posteriorcomp4$beta7))
+posteriorcomp4 <- posteriorcomp4 %>% 
+  mutate(variable = c(rep("Edad", 5400), rep("Sexo", 5400), rep("Resonador GE", 5400), rep("Resonador Philips", 5400), rep("Intensidad", 5400)))
 
-ggplot(posteriorcomp4) + geom_density(aes(x = beta5))
+ggplot(posteriorcomp4) + stat_summary(aes(y = variable, x = b1), fun.data = mean_sdl, color = "#057057") + 
+  geom_vline(xintercept = 0, linetype = 2) + labs(y = "Variable", x = "Coeficiente") + theme_minimal()
+
 
